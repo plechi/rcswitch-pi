1a618497c5cced06a86505023c5de12e225aeefc
Plechi <github@plechinger.at>
2016-09-04 00:38:31 +0200

    aaaaaahhhh

--- piInterrupt.h
+++ piInterrupt.h
@@ -2,6 +2,15 @@
 #define CHANGE INT_EDGE_BOTH
 #endif
 
+// necessary values to port rc-switch to the raspberry pi
+#ifndef boolean
+#define boolean int
+#endif
+
+#ifndef byte
+#define byte char
+#endif
+
 // replace detachInterrupt() with a empty block
 #define detachInterrupt(x) do { } while(0)
 
