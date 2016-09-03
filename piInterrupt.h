2ac05082d2418c5476bf3f7699e0d9e773c67cea
Plechi <github@plechinger.at>
2016-09-04 00:41:59 +0200

    test

--- piInterrupt.h
+++ piInterrupt.h
@@ -15,7 +15,5 @@
 #define detachInterrupt(x) do { } while(0)
 
 // map attachInterrupt() to wiringPi equivalent
-#define attachInterrupt(pin, handler, edgeType)		\
-		do {	 									\
-			wiringPiISR(pin, edgeType, &handler);	\
-		} while(0)
+#define attachInterrupt(pin, handler, edgeType) do {wiringPiISR(pin, edgeType, &handler);} while(0)
+
