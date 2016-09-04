753d996f103863041f0f775f3e080eb0871319f8
Plechi <github@plechinger.at>
2016-09-04 18:45:05 +0200

    flush

--- receive.cpp
+++ receive.cpp
@@ -43,6 +43,7 @@ int main(int argc, char *argv[]) {
             } else {
                 printf("Unknown encoding\n");
             }
+            fflush(stdout);
             receiver.resetAvailable();
         }
 	}
